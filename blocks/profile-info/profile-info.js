15a2aa183d9aa4233154f8b8fad9573899d7041c
dvsolodov <d.v.solodov@yandex.ru>
2021-12-09 15:37:40 +1000

    feat: optimize popup code

--- blocks/profile-info/profile-info.js
+++ blocks/profile-info/profile-info.js
@@ -1,16 +1,17 @@
-let editPopup = document.querySelector('#edit-popup');
 let profileInfoEditBtn = document.querySelector('#profile-info-edit-btn');
+let editPopup = document.querySelector('#edit-popup');
 let form = document.querySelector('#form');
 let btnCloseForm = form.querySelector('#btn-close-form');
+let nameInput = form.querySelector('#input-name');
+let aboutInput = form.querySelector('#input-about');
+let profileInfoName = document.querySelector('#profile-info-name');
+let profileInfoAbout = document.querySelector('#profile-info-about');
 
 function openEditPopup(evt) {
   evt.preventDefault();
 
-  let nameInput = form.querySelector('#input-name');
-  let aboutInput = form.querySelector('#input-about');
-
-  nameInput.value = document.querySelector('#profile-info-name').textContent;
-  aboutInput.value = document.querySelector('#profile-info-about').textContent;
+  nameInput.value = profileInfoName.textContent;
+  aboutInput.value = profileInfoAbout.textContent;
 
   editPopup.classList.toggle('popup_open');
 }
@@ -24,14 +25,8 @@ function closeEditPopup(evt) {
 function formSubmitHandler(evt) {
   evt.preventDefault();
 
-  let nameInput = form.querySelector('#input-name').value;
-  let aboutInput = form.querySelector('#input-about').value;
-
-  let profileInfoName = document.querySelector('#profile-info-name');
-  let profileInfoAbout = document.querySelector('#profile-info-about');
-
-  profileInfoName.textContent = nameInput;
-  profileInfoAbout.textContent = aboutInput;
+  profileInfoName.textContent = nameInput.value;
+  profileInfoAbout.textContent = aboutInput.value;
 
   closeEditPopup(evt);
 }
