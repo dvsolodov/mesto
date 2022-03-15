f8860513ccd064a3dc2e34daefe629d44c4484f0
dvsolodov <d.v.solodov@yandex.ru>
2022-03-15 22:41:53 +1000

    feat: add getProfile, editProfile, getInitialCards to Api class

--- src/components/Api.js
+++ src/components/Api.js
@@ -2,6 +2,7 @@ export default class Api {
   constructor(options) {
     this._baseUrl = options.url;
     this._token = options.token;
+    this._paramProfile = "users/me";
     this._paramCards = "cards";
     this._headers = {
       authorization: this._token,
@@ -9,15 +10,29 @@ export default class Api {
     };
   }
 
-  getInitialCards() {
-    return fetch(`${this._baseUrl}${this._paramCards}`, {headers: this._headers})
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          return Promise.reject(`Ошибка получения ответа от сервера: ${response.status}`);
-        }
-      })
+  getProfile() {
+    return fetch(this._baseUrl + this._paramProfile, {headers: this._headers})
+      .then((response) => this._checkResponse(response))
       .catch(err => console.log(err));
+  }
+
+  editProfile({name, about}) {
+    return fetch(this._baseUrl + this._paramProfile, {
+        method: "PATCH",
+        headers: this._headers,
+        body: JSON.stringify({name, about})
+      })
+      .then((response) => this._checkResponse(response))
+      .catch(err => console.log(err));
+  }
+
+  getInitialCards() {
+    return fetch(this._baseUrl + this._paramCards, {headers: this._headers})
+      .then((response) => this._checkResponse(response))
+      .catch(err => console.log(err));
+  }
+
+  _checkResponse(response) {
+    return response.ok ? response.json() : Promise.reject(`Ошибка запроса: ${response.status}`);
   }
 }
