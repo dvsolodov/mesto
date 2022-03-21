0125faf6504c1ef223302cff4f1925c6fda8000d
dvsolodov <d.v.solodov@yandex.ru>
2022-03-21 11:06:19 +1000

    hotfix: use mousedown event to close popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -28,7 +28,7 @@ export default class Popup {
   }
 
   setEventListeners() {
-    this._popup.addEventListener('click', (evt) => {
+    this._popup.addEventListener('mousedown', (evt) => {
       if (evt.target.classList.contains('popup') || evt.target.classList.contains('popup__close-btn')) {
         this.close();
       }
